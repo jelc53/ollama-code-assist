cf274efd2d3b5b23cfe842f578c226bace1e4289
Julian Cooper <julian.e.cooper2@gmail.com>
2025-01-18 19:39:50 +0100

    update docker cmd to listen across local ports

--- run_webui.sh
+++ run_webui.sh
@@ -1,1 +1,2 @@
 docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+sudo docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
